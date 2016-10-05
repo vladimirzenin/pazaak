52058a38b399c1ca27e3b0610a713d3231eae86e
Vladimir Zenin <Vladimir Zenin>
2016-10-06 00:51:02 +0300

    3

--- pazaak.py
+++ pazaak.py
@@ -28,31 +28,91 @@ import array
 import random
 
 def main():
-    print('Starting pazaak math')
+    
+    init_math = True
 
-    cards           = array.array('H') # Главная колода
-    handles_player1 = array.array('H') # Рука игрока 1
-    handles_player2 = array.array('H') # Рука игрока 2
+    while True:
 
-    board_player1   = array.array('H', [0,0,0,0,0,0,0,0,0]) # Доска игрока 1
-    board_player2   = array.array('H', [0,0,0,0,0,0,0,0,0]) # Доска игрока 2
+        if init_math:
+            print('Starting pazaak math')
 
-    score_b_pl1     = 0                # Счет игрока 1
-    score_b_pl2     = 0                # Счет игрока 2
+            cards           = array.array('H') # Главная колода
+            handles_player1 = array.array('H') # Рука игрока 1
+            handles_player2 = array.array('H') # Рука игрока 2
 
-    full_score_pl1  = 0                # Выиграно партий игроком 1
-    full_score_pl2  = 0                # Выиграно партий игроком 2
+            board_player1   = array.array('H', [0,0,0,0,0,0,0,0,0]) # Доска игрока 1
+            board_player2   = array.array('H', [0,0,0,0,0,0,0,0,0]) # Доска игрока 2
 
-    init_cards(cards, handles_player1, handles_player2)
+            score_b_pl1     = 0                # Счет игрока 1
+            score_b_pl2     = 0                # Счет игрока 2
 
-    cancel_round = False
-    while not cancel_round:
+            full_score_pl1  = 0                # Выиграно партий игроком 1
+            full_score_pl2  = 0                # Выиграно партий игроком 2
+
+            init_cards(cards, handles_player1, handles_player2)
+
+            init_math = False
+        
+        print('Turn of Player 1:')
         step(board_player1, cards, handles_player1, score_b_pl1)
-        step(board_player2, cards, handles_player2, score_b_pl2)
-        cancel_round = True
 
+        print('Turn of Player 2:')
+        step(board_player2, cards, handles_player2, score_b_pl2)
+
+        status_pl1 = checkWin(score_b_pl1)
+        status_pl2 = checkWin(score_b_pl2)
+
+        win_message = ''
+
+        if status_pl1 == 2 and status_pl2 == 2:
+            # Next turn
+            win_message = ''
+        elif (status_pl1 == 0 and status_pl2 == 0
+            or status_pl1 == 1 and status_pl2 == 1):
+            # Standoff
+            win_message = 'Standoff turn!'
+            init_math = True            
+        elif status_pl1 == 1:
+            # win player 1
+            full_score_pl1 = full_score_pl1 + 1
+            win_message = 'Player 1 win turn!'
+            init_math = True
+        elif status_pl2 == 1:
+            # win player 2
+            full_score_pl2 = full_score_pl2 + 1
+            win_message = 'Player 2 win turn!'
+            init_math = True
+        elif status_pl1 == 2:
+            # player 1 - lose; win player 2
+            full_score_pl2 = full_score_pl2 + 1
+            win_message = 'Player 2 win turn!'
+            init_math = True
+        elif status_pl2 == 2:
+            # player 2 - lose; win player 1
+            full_score_pl1 = full_score_pl1 + 1
+            win_message = 'Player 1 win turn!'
+            init_math = True
+
+        print(win_message)
+
+        win_message = ''
+        if full_score_pl1 == 3 and full_score_pl2 == 3:
+            # Standoff math
+            win_message = 'Standoff math!'
+            break
+        elif full_score_pl1 == 3:
+            # Player 1 win math
+            win_message = 'Player 1 win math!'
+            break
+        elif full_score_pl2 == 3:
+            # Player 2 win math
+            win_message = 'Player 2 win math!'
+            break
+
+        print(win_message)
+
+# Инициализация колод
 def init_cards(cards, handles_player1, handles_player2):
-    print('Select cards...')
 
     i = 1
     while i <= 4:
@@ -73,44 +133,59 @@ def init_cards(cards, handles_player1, handles_player2):
         handles_player2.append(x) 
         m = m + 1  
 
+# Ход игрока
 def step(board, cards, handles_card, score):
     
-    ind = 0
-    ind_board = -1
-    while ind <= 8:
-        if board[ind] == 0:
-            ind_board = ind
-            break
-        ind = ind + 1 
-
+    # Вытягивание карты из главной колоды
     ind_selected_card_general = random.randint(0, len(cards) )
     selected_card_general = cards[ind_selected_card_general]
     cards.pop(ind_selected_card_general)
 
     score = score + selected_card_general
-
+    
+    # Даем возможность сбросить карту руки, либо завершить ход, либо пасовать
     hand_gamer = ''
     for i in handles_card:
-        hand_gamer = hand_gamer + ', ' + str(i)
+        hand_gamer = (hand_gamer 
+                    + (', ' if len(hand_gamer) > 0 else '')
+                    + str(i))
+    str_score = 'Your score: ' + str(score) + '. Your hand: ' + hand_gamer + '. Enter the hand card, or "Next", or "Pass"'
 
-    str_ = 'Your score: ' + str(score) + '. Доступна рука игрока: ' + hand_gamer
-    print(str_)
-    select_done = False
-    while not select_done:
+    while True:
+        
+        print(str_score)
 
         user_input = input()
-        print('======================')
-        print(user_input)
-        select_card = int(user_input)
-        ind = handles_card.index(select_card)
-        if ind > 0:
+        
+        try:
+            user_input = int(user_input)
+        except:
+            pass
+
+        if user_input == "Next" or user_input == "next":
+            print('End turn.')
+            break
+        elif user_input == "Pass" or user_input == "pass":
+            # ДОПИСАТЬ
+            break
+        elif user_input in handles_card:
+            select_card = int(user_input)
+            score = score + select_card
+            str_score = 'Your score: ' + str(score) + '. End turn.'
+            print(str_score)
+
+            ind = handles_card.index(select_card)
             handles_card.pop(ind)
-            select_done = True
+            break
 
-    
-            
-
-
+# Проверка статуса - поражение, победа, или в процессе игры
+def checkWin(score):
+    if score > 20:
+        return 0 # Fail
+    elif score == 20:
+        return 1 # Win
+    else:
+        return 2 # Not win
 
 if __name__ == "__main__":
     main()
